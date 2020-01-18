d3ec316740d547837546125d46ab1aa2dfcfb1c7
Hamidreza Niazi <hamidrezaniazi@yahoo.com>
2020-01-18 19:59:00 +0330

    Add guard detection

---
+++ src/Guard.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Hamidrezaniazi\Upolo;
+
+use Illuminate\Support\Facades\Config;
+
+class Guard
+{
+    /**
+     * Return guard model class name.
+     *
+     * @return string
+     */
+    public static function getGuardClassName(): string
+    {
+        $guard = Config::get('auth.defaults.guard');
+        $guard = collect(config('auth.guards'))
+            ->map(function ($guard) {
+                if (! isset($guard['provider'])) {
+                    return;
+                }
+
+                return config("auth.providers.{$guard['provider']}.model");
+            })->get($guard);
+
+        return class_exists($guard) ? $guard : $guard = \Hamidrezaniazi\Upolo\Tests\Model\User::class;
+    }
+}
