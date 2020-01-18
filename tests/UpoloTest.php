717042878f9edc6ffee91b630c043a994553eb07
Hamidreza Niazi <hamidrezaniazi@yahoo.com>
2020-01-18 19:59:20 +0330

    Add test example

---
+++ tests/UpoloTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Hamidrezaniazi\Upolo\Tests;
+
+use Hamidrezaniazi\Upolo\Tests\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UpoloTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function userCanUploadFile()
+    {
+        //
+    }
+}
