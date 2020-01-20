5ed933b32dc26a6668ea3ffa023d01c1bc1d1720
Hamidreza Niazi <hamidrezaniazi@yahoo.com>
2020-01-21 01:50:21 +0330

    Remove extras

--- src/UpoloServiceProvider.php
+++ src/UpoloServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace Hamidrezaniazi\Upolo;
 
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class UpoloServiceProvider extends ServiceProvider
@@ -12,12 +11,6 @@ class UpoloServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__ . '/../config/config.php' => config_path('upolo.php'),
-            ], 'config');
-        }
-
         if (!class_exists('CreateModelHistoriesTable')) {
             $this->publishes([
                 __DIR__ . '/../database/migrations/create_files_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . 'create_files_table.php'),
@@ -30,10 +23,6 @@ class UpoloServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'upolo');
-
-        // Register the main class to use with the facade
         $this->app->singleton('upolo', function () {
             return new Upolo();
         });
