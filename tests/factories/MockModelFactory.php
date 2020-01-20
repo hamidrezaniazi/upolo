bdbbfbc78bb4a3ae368cc3b3c6d9da443dc336e5
Hamidreza Niazi <hamidrezaniazi@yahoo.com>
2020-01-21 01:50:08 +0330

    Add factory

---
+++ tests/factories/MockModelFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+/* @var $factory Factory */
+
+use Illuminate\Database\Eloquent\Factory;
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(\Hamidrezaniazi\Upolo\Tests\Models\MockModel::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
