19100c544aee01247731d9fa5794b7b22a3ca3f2
Hamidreza Niazi <hamidrezanaizi@yahoo.com>
2020-01-22 10:40:26 +0330

    Fix published migration name

--- src/UpoloServiceProvider.php
+++ src/UpoloServiceProvider.php
@@ -13,7 +13,7 @@ class UpoloServiceProvider extends ServiceProvider
     {
         if (! class_exists('CreateModelHistoriesTable')) {
             $this->publishes([
-                __DIR__.'/../database/migrations/create_files_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'create_files_table.php'),
+                __DIR__.'/../database/migrations/create_files_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_files_table.php'),
             ], 'migrations');
         }
     }
